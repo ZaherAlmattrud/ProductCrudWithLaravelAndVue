09fbfddbc6b99cca696ac9f1ea70ba1626363392
zaheralmattrud <zaheralmattrud@gmail.com>
2024-02-17 19:08:32 +0200

    add products cruds component and create routes and init axios

---
+++ resources/js/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1 class="alert alert-danger text-center">Laravel Vue Product Crud</h1>
+    <div class="container">
+        <router-view></router-view>
+    
+      <!-- <router-link to="/"  >Products</router-link> |
+      <router-link to="/create"  >Add Product</router-link> | -->
+ 
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "App",
+  };
+  </script>
+  
+            
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,17 +1,18 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" value="{{csrf_token()}}" />
-        <title>Vue Laravel Product Crud</title>
-        <link href="{{mix('app.css')}}" type="text/css" rel="stylesheet" />
 
-     
-    </head>
-    <body >
-        <div id="app"></div>
-        <script src="{{mix('js/app.js')}}" type="text/javascript"></script>
-   
-    </body>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel 10 vite with vue</title>
+
+</head>
+
+<body>
+    <div id="app"></div>
+    @vite('resources/js/app.js')
+</body>
+
 </html>
+          
